0c40f52769479506dbd33097b8427ced2daa5a90
Tatyana Gankin <125816651+tanyagankin@users.noreply.github.com>
2023-07-02 01:35:15 +0300

    Update HW_2.sql

--- HW_2.sql
+++ HW_2.sql
@@ -12,9 +12,9 @@ CREATE TABLE public.students (
 select * from students s ;
 
 --2)Display all students from the table 
-select id,name from students;
+select id, name from students;
 
---3)Display only use ID  
+--3)Display only user ID  
 select id from students s ;
 
 --4)Display only the username
@@ -26,23 +26,23 @@ select email from students s ;
 --6)Display username and email
 select name, email from students s ;
 
---7)Display id,name,email and creation date of users
+--7)Display id, name, email and creation date of users
 select id, name, email, created_on  from students;
 
 --8)Display users where password is 12333
-select id,name,password from students
+select id, name, password from students
 where password ='12333';
 
 --9)Display users who were creared 2021-03-26 00:00:00
-select id,name from students s
+select id, name, created_on from students s
 where created_on ='2021-03-26 00:00:00';
 
 --10)Display users where the name contains the word Anna
-select id,name from students
+select id, name from students
 where name like '%Anna%';
 
 --11)Display users where is 8 in the name at the end
-select id,name from students 
+select id, name from students 
 where name like '%8';
 
 --12)Display users where in the name there is a letter "a"
@@ -50,7 +50,7 @@ select id, name from students s
 where name like '%a%';
 
 --13)Display users who were created 2021-07-12 00:00:00
-select id,name, created_on from students s
+select id, name, created_on from students s
 where created_on = '2021-07-12 00:00:00';
 
 --14)Display users who were created on 2021-07-12 00:00:00 and have a password of 1m313
@@ -59,46 +59,46 @@ where created_on ='2021-07-12 00:00:00'
 and password ='1m313';
 
 --15)Display users who were created on 2021-07-12 00:00:00 and whose name contains the word Andrey
-select id,name from students 
+select id, name from students 
 where created_on ='2021-07-12 00:00:00'
 and name like '%Andrey%';
 
 --16)Display users who were created on 2021-07-12 00:00:00 and who have the number 8 in their name 
-select id ,name from students s
+select id, name from students s
 where created_on ='2021-07-12 00:00:00'
 and name like '%8%';
 
 --17)Display the user whose id is 110
-select id,name from students s 
+select id, name from students s 
 where id = 110;
 
 --18)Display the user whose id is 153;
-select id,name from students s
+select id, name from students s
 where id = 153;
 
 --19)Display user whose id is greater then 140
-select id,name from students s  
+select id, name from students s  
 where id > 140;
 
---20)Dispay user whose id is less than 130
-select id,name from students s 
+--20)Display user whose id is less than 130
+select id, name from students s 
 where id < 130;
 
---21)Display user whose id is less than 127 or grater than 188
-select id ,name from students s
+--21)Display user whose id is less than 127 or greater than 188
+select id , name from students s
 where id < 127 or id > 188;
 
---22)Display the user whose id is less than or egual to 137
-select id ,name from students 
+--22)Display the user whose id is less than or equal to 137
+select id, name from students 
 where id <= 137;
 
---23)Display the user whose id is greater than or egual to 137
-select id ,name from students
+--23)Display the user whose id is greater than or equal to 137
+select id , name from students
 where id >=137;
 
 --24)Display a user whose id is greater than 180 but less than 190
 select id, name from students s
-where id > 180 and < 190;
+where id > 180 and id < 190;
 
 --25)Display user whose id is between 180 and 190
 select id, name from students s
@@ -106,18 +106,18 @@ where id between 180 and 190;
 
 --26)Display users where the password is 12333,1m313,123313
 select id,name from student 
-where password = '12333','1m313','123313';--?
+where password = '12333' or password ='1m313' or password = '123313';
 
 select * from students s 
 where password in ('12333','1m313','123313');
 
---27)Display users where created_on egual 2020-10-03 00:00:00,2021-05-19 00:00:00,2021-03-26 00:00:00
-select id ,name from students s 
+--27)Display users where created_on equal 2020-10-03 00:00:00,2021-05-19 00:00:00,2021-03-26 00:00:00
+select id, name from students s 
 where created_on in('2020-10-03 00:00:00','2021-05-19 00:00:00','2021-03-26 00:00:00');
 
-select id ,name from students
-where created_on = '2020-10-03 00:00:00',
-	or created_on = '2021-05-19 00:00:00',
+select id, name from students
+where created_on = '2020-10-03 00:00:00'
+	or created_on = '2021-05-19 00:00:00'
 	or created_on = '2021-03-26 00:00:00';
 
 --28)Display the minimum id
@@ -132,14 +132,14 @@ from students s ;
 select count(id)
 from students s ;
 
---31)Display user id,name,user creation date .
+--31)Display user id, name, user creation date.
 --Sort in ascending order by the date the user was added
 select id, name, created_on
 from students s 
 order by created_on asc ;
 
---32)Display user id,name,user creation date.
+--32)Display user id, name, user creation date.
 --Sort in descending order by the date the user was added 
-select id, name , created_on
+select id, name, created_on
 from students s 
 order by created_on desc;
